5d2db5a106058f211bf7108867af96e98fd32348
Tony Park <tonypark0403@gmail.com>
2020-04-29 14:00:49 -0400

    added chat and message

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -7,10 +7,14 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import config from '../config';
+import Chat from './Chat';
+import Message from './Message';
 
 @Entity()
 class User extends BaseEntity {
@@ -65,6 +69,12 @@ class User extends BaseEntity {
 
   @Column({ type: 'double precision', default: 0 })
   lastOrientation: number;
+
+  @ManyToOne((type) => Chat, (chat) => chat.participants)
+  chat: Chat;
+
+  @OneToMany((type) => Message, (message) => message.user)
+  messages: Message[];
 
   @CreateDateColumn()
   createdAt: string;
