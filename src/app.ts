1481f0e4eaf2a233c14c19fe46b38b358f32dc1c
Tony Park <tonypark0403@gmail.com>
2020-05-01 11:24:52 -0400

    added decodeJWT and putting express data into graphql

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import cors from 'cors';
+import { NextFunction, Response } from 'express';
 import { GraphQLServer } from 'graphql-yoga';
 import helmet from 'helmet';
 import logger from 'morgan';
+import config from './config';
 import schema from './schema';
+import decodeJWT from './utils/decodeJWT';
 
 class App {
   private middlewares = (): void => {
@@ -10,12 +13,42 @@ class App {
     this.app.express.use(cors());
     this.app.express.use(logger('dev'));
     this.app.express.use(helmet());
+    this.app.express.use(this.jwt);
   };
+
+  private jwt = async (
+    req,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const token = req.get(config.TOKEN_COOKIE);
+    if (token) {
+      const user = await decodeJWT(token);
+      if (user) {
+        req.user = user;
+        // console.log('user by token:', req);
+      } else {
+        req.user = undefined;
+      }
+    }
+    next();
+  };
+
   public app: GraphQLServer;
   constructor() {
     this.app = new GraphQLServer({
       // option
       schema,
+      context: (express) => {
+        // console.log('context:', express.request);
+        return {
+          req: express.request,
+        };
+      },
+      // context: { // second parameter를 통해서 data를 graphql에 context로 보낼수 있음
+      // developer: "Tony Park" // 이렇게 하면 이 정보가 resolvers에 3번째 파라미터로 넘어감
+      // 또한 위에처럼 function도 가능
+      // }
     });
     this.middlewares();
   }
---
+++ src/utils/decodeJWT.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import config from '../config';
+import User from '../entities/User';
+
+const decodeJWT = async (token: string): Promise<User | undefined> => {
+  try {
+    const decoded: any = jwt.verify(token, config.JWT_SECRET);
+    // console.log('decoded token:', decoded);
+    const { id } = decoded;
+    const user = await User.findOne({ id });
+    return user;
+  } catch (error) {
+    return undefined;
+  }
+};
+export default decodeJWT;
