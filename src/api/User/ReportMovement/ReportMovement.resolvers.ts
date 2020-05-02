d14d33279fce20b574ce49529a7c963e26ad763e
Tony Park <tonypark0403@gmail.com>
2020-05-02 15:46:14 -0400

    added report movement

---
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -0,0 +1,36 @@
+import { Resolvers } from 'src/types/resolvers';
+import User from '../../../entities/User';
+import {
+  ReportMovementMutationArgs,
+  ReportMovementResponse,
+} from '../../../types/graph';
+import cleanNullArgs from '../../../utils/clearnNullArgs';
+import privateResolver from '../../../utils/privateResolver';
+const resolvers: Resolvers = {
+  Mutation: {
+    ReportMovement: privateResolver(
+      async (
+        _,
+        args: ReportMovementMutationArgs,
+        { req }
+      ): Promise<ReportMovementResponse> => {
+        const user: User = req.user;
+        const notNull = cleanNullArgs(args);
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+          };
+        }
+      }
+    ),
+  },
+};
+
+export default resolvers;
