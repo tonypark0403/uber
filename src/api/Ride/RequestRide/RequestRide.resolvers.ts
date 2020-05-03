d643704a0cbf8781a56851621da6f38fbf3364c7
Tony Park <tonypark0403@gmail.com>
2020-05-02 23:19:02 -0400

    fixed isRiding part

--- src/api/Ride/RequestRide/RequestRide.resolvers.ts
+++ src/api/Ride/RequestRide/RequestRide.resolvers.ts
@@ -16,23 +16,33 @@ const resolvers: Resolvers = {
         { req, pubSub }
       ): Promise<RequestRideResponse> => {
         const user: User = req.user;
-        try {
-          const ride: Ride = await Ride.create({
-            ...args,
-            passenger: user,
-          }).save();
-          pubSub.publish(config.SUBSCRIPTION_CHANNEL.RIDEREQUEST, {
-            [config.SUBSCRIPTION.NEARBYRIDESUBSCRIPTION]: ride,
-          });
-          return {
-            ok: true,
-            error: null,
-            ride,
-          };
-        } catch (error) {
+        if (!user.isRiding) {
+          try {
+            const ride: Ride = await Ride.create({
+              ...args,
+              passenger: user,
+            }).save();
+            pubSub.publish(config.SUBSCRIPTION_CHANNEL.RIDEREQUEST, {
+              [config.SUBSCRIPTION.NEARBYRIDESUBSCRIPTION]: ride,
+            });
+            user.isRiding = true;
+            user.save();
+            return {
+              ok: true,
+              error: null,
+              ride,
+            };
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+              ride: null,
+            };
+          }
+        } else {
           return {
             ok: false,
-            error: error.message,
+            error: "'You can't request two rides",
             ride: null,
           };
         }
